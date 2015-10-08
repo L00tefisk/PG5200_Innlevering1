dee18760096569bb13e136ba97243adbbaad4a20
L00tefisk <Eivind.93@hotmail.com>
2015-10-08 23:17:04 +0200

    Added ability to add and remove items from the list

--- PG5200_Innlevering1.SettingsEditor/ViewModel/MainViewModel.cs
+++ PG5200_Innlevering1.SettingsEditor/ViewModel/MainViewModel.cs
@@ -6,6 +6,7 @@ using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using PG5200_Innlevering1.SettingsEditor.Model;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace PG5200_Innlevering1.SettingsEditor.ViewModel
 {
@@ -29,24 +30,26 @@ namespace PG5200_Innlevering1.SettingsEditor.ViewModel
                 _level.Enemies = value;
             }
         }
-        private Enemy _currentEnemy { get; set; }
-        public Enemy CurrentEnemy {
+        private Enemy _selectedItem { get; set; }
+        public Enemy SelectedItem {
             get
             {   
-                return _currentEnemy;
+                return _selectedItem;
             }
             set
             { 
-                _currentEnemy = value;
+                _selectedItem = value;
                 RaisePropertyChanged();
             }
         }
 
         #region Commands
 
-        public ICommand SaveCommand { get; set; }
-        public ICommand LoadCommand { get; private set; }
         public ICommand NewCommand { get; private set; }
+        public ICommand RemoveCommand { get; private set; }
+        public ICommand DefaultCommand { get; private set; }
+        public ICommand SaveCommand { get; private set; }
+        public ICommand LoadCommand { get; private set; }
 
         #endregion
 
@@ -56,24 +59,44 @@ namespace PG5200_Innlevering1.SettingsEditor.ViewModel
         public MainViewModel()
         {
             _level = new Level();
-            _currentEnemy = new Enemy();
+            _selectedItem = new Enemy();
             CreateCommands();
         }
 
         private void CreateCommands()
         {
-            NewCommand = new RelayCommand(NewCharacter);
+            NewCommand = new RelayCommand(AddItem);
+            RemoveCommand = new RelayCommand(RemoveItem);
+            DefaultCommand = new RelayCommand(DefaultLevel);
             SaveCommand = new RelayCommand(Save, CanSave);
             LoadCommand = new RelayCommand(Load);
         }
-
+        public void AddItem()
+        {
+            Enemies.Add(
+                new Enemy() {
+                    Name = "New Enemy",
+                    Health = 100,
+                    MoveSpeed = 2.5f,
+                    Damage = 10,
+                    AttackSpeed = 0.5f,
+                    ScoreValue = 10,
+                    SpawnTime = 3,
+                }
+            );
+            SelectedItem = Enemies.Last();
+        }
+        public void RemoveItem()
+        {
+            Enemies.Remove(_selectedItem);
+        }
         private bool CanSave()
         {
             //TODO: Additional validation
             return true;
         }
 
-        public void NewCharacter()
+        public void DefaultLevel()
         {
             PopulateView(new Level());
         }
